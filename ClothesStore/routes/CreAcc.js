1c0c58417fd2a4b5328caef4f4f087b087b66581
danhnc2k <55302957+danhnc2k@users.noreply.github.com>
2020-12-08 10:13:10 +0700

    Update Client Store New Template
    Đã thêm các chức năng:
    +Danh sách tất cả sản phẩm
    +Danh sách sản phẩm lọc theo phân loại
    +Tìm kiếm sản phẩm theo tên

--- ClothesStore/routes/CreAcc.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const CreAccController = require('../controllers/CreAccController');
-
-/* GET list of books. */
-router.get('/', CreAccController.index);
-
-
-module.exports = router;
--- ClothesStore/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const loginController = require('../controllers/loginController');
-
-/* GET list of books. */
-router.get('/', loginController.index);
-
-
-module.exports = router;
