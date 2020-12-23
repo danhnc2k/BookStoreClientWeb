900977a0ed55fd21f026b1176aab3331988e12e8
ducnghu <55783340+ducnghu@users.noreply.github.com>
2020-12-23 14:43:47 +0700

    sửa đổi trang chủ và thêm một số sắp xếp ds sản phẩm

--- models/productService.js
+++ models/productService.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 exports.count = (filter = {}) => Product.find(filter).countDocuments();
 
-exports.listproduct = (filter = {}, page = 1, itemPerPage = 12, sort = null) => {
-    const res = Product.paginate(filter, {page: page, limit: itemPerPage, sort: {price: sort},});
+exports.listproduct = (filter = {}, page = 1, itemPerPage = 12, sortObject) => {
+    const res = Product.paginate(filter, {page: page, limit: itemPerPage, sort: sortObject,});
     return res;
 };
